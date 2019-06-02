c6fabef1e0dd568a0b829f0123cc2cf929d7891b
Johnny <zhaozy93@outlook.com>
2019-06-02 16:37:41 +0800

    Update 28.实现str-str.go

--- source-go/28.实现str-str.go
+++ source-go/28.实现str-str.go
@@ -3,29 +3,46 @@
  *
  * [28] 实现strStr()
  */
-func strStr(haystack string, needle string) int {
-	if len(needle) == 0 {
-		return 0
-	}
-	if len(needle) > len(haystack) {
-		return -1
-	}
-	j := 0
-	i := 0
-	for i = 0; i < len(haystack); i++ {
-		if j == len(needle) {
-			return i - len(needle)
+// 使用KMP算法
+func strStr(s string, p string) int {
+    if len(p) == 0{
+        return 0 
+    }
+    pp := make([]int, len(p))
+	pp[0] = 0
+	for i := 1; i < len(p); i++ {
+		j := pp[i-1]
+		for {
+			if p[i] == p[j] {
+				pp[i] = j + 1
+				break
+			} else {
+				if j == 0 { 
+					pp[i] = 0
+					break
+				}
+				j = pp[j-1]
+			}
 		}
-		if haystack[i] == needle[j] {
+	}
+
+	i, j := 0, 0
+	for i < len(s) && j < len(p) {
+		if s[i] == p[j] {
+			i++
 			j++
 		} else {
-			i -= j
-			j = 0
+			if j == 0 {
+				i++
+				continue
+			}
+			j = pp[j-1]
 		}
 	}
-	if j == len(needle) {
-		return i - len(needle)
+	if j == len(p) {
+		return i - j 
 	}
 	return -1
 }
+
 
